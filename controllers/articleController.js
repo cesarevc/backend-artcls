8742cfb170f35ec2bb4819e9459d033d41678e0d
Vazquez Cesar <cesar.evc@outlook.com>
2020-05-05 17:14:33 -0500

    article api

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -1,18 +1,69 @@
 'use strict'
 
+var validator = require('validator');
+
+var Article = require('../models/article');
+
 var controller = {
     datos :  (req, res) => {
         return res.status(200).send({
-            curso: "backend",
-            autor: "cesar enrique",
-            web: "www.cesc.com"
+            curso: 'backend',
+            autor: 'cesar enrique',
+            web: 'www.cesc.com'
         });
     },
 
     test: (req, res) => {
         return res.status(200).send({
-            message: ' Soy la accion test',
+            message: 'Soy la accion test',
         });
+    },
+
+    save: (req, res) => {
+        //recive post
+        var params = req.body;
+        //validate data
+        try {
+
+            var validate_title = !validator.isEmpty(params.title);
+            var validate_content = !validator.isEmpty(params.content);
+
+        } catch (err) {
+            return res.status(200).send({
+                message: 'Hubo un error, faltan datos',
+            });
+        }
+
+        if (validate_title && validate_content){
+           
+            //create object to save
+            var article = new Article();
+            //assign values
+            article.title = params.title;
+            article.content = params.content;
+            article.image = null;
+            //save article in bd
+            article.save((err, articleStored) => {
+                if(err || !articleStored){
+                    return res.status(404).send({
+                        status: 'error',
+                        message: 'el articulo no se ah guardado',
+                    });
+                }
+
+                //response to request
+                return res.status(200).send({
+                    status: 'success',
+                    article
+                });
+            });
+        } else {
+            return res.status(200).send({
+                status: 'error',
+                message: 'los datos no son validos',
+            }); 
+        }
+
     }
 }
 
